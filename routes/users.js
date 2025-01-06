f5cf5f78dedaa2adb9576e4400b28f91c4c505ad
DeanNguyen98 <nctuan.hn@gmail.com>
2025-01-06 22:42:55 +1100

    setting up routers

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const pg = require("pg");
+const router = require("express").Router();
+
+
+router.post("/login", function (req, res, next) {});
+
+
+router.post("/register", function (req, res, next) {});
+
+module.exports = router;
