7d4128934589d9513e139b4a26eee93d44c9a437
DeanNguyen98 <nctuan.hn@gmail.com>
2025-01-07 00:27:43 +1100

    update user routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,33 @@
-const pg = require("pg");
 const router = require("express").Router();
-
-
+const pool = require("../config/database")
+const utils = require("../lib/util")
 router.post("/login", function (req, res, next) {});
 
+router.get("/main", (req,res) => {
+    res.send("hello");
+})
+router.post("/register", async function (req, res, next) {
+    const saltHash = utils.genPassword(req.body.password);
 
-router.post("/register", function (req, res, next) {});
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+    const user = {
+        username: req.body.username,
+        hash: hash,
+        salt: salt,
+      };
+
+  try {
+    await pool.query("INSERT INTO users (username, salt, hash) VALUES ($1, $2, $3)", [
+        req.body.username,
+        salt,
+        hash
+      ]);
+      res.json({success:true, user:user});
+  } catch(err) {
+    console.error("Error saving user:", err);
+    res.json({ success: false, msg: err });
+}
+});
 
 module.exports = router;
