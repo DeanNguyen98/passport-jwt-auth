1d228d1c7c51b7120657bb0907a7aa92387822cf
DeanNguyen98 <nctuan.hn@gmail.com>
2025-01-14 22:32:12 +1100

    modify protected route

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,32 @@
-
-import './App.css'
-
-import AppRoutes from "./routes/Routes";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ProtectedRoute from "./components/Protected";
 
 const App = () => {
-  return <AppRoutes />;
+  const NavigationButtons = () => {
+    const navigate = useNavigate();
+
+    return (
+      <div>
+        <button onClick={() => navigate("/login")}>Login</button>
+        <button onClick={() => navigate("/register")}>Register</button>
+      </div>
+    );
+  };
+
+  return (
+    <Router>
+      <div>
+        <NavigationButtons />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/protected" element={<ProtectedRoute />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
